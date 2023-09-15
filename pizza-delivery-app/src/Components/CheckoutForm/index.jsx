f1963ba03d1740f3f138171cc0cc9e483c8b8550
Raquel Illerias <raquel.illerias@gmail.com>
2023-09-15 14:36:09 +0100

    CheckoutForm tests added

--- pizza-delivery-app/src/Components/CheckoutForm/index.jsx
+++ pizza-delivery-app/src/Components/CheckoutForm/index.jsx
@@ -5,22 +5,22 @@ export default function CheckoutForm({ basket }) {
 
     return (
         <>
-            <FormContainer>
+            <FormContainer data-testid='component-container'>
 
                 <TitleBox>
                     <Ornament src={require("../../images/ornament-pizza-2.webp")} alt="" />
-                    <Title variant="h3" component={"h2"} mt={1.5} sx={{ fontFamily: 'Roboto Condensed, sans-serif', fontWeight: 'bold' }}>
+                    <Title variant="h3" component={"h2"} mt={1.5} sx={{ fontFamily: 'Roboto Condensed, sans-serif', fontWeight: 'bold' }} data-testid='title-text'>
                         MY DETAILS
                     </Title>
                     <Ornament src={require("../../images/ornament-pizza-2.webp")} alt="" />
                 </TitleBox>
                 <Container maxWidth="sm">
                     <FormWrapper>
-                        <TextFieldStyled id="outlined-basic" label="Name" size="small" color="secondary" variant="outlined" required />
-                        <TextFieldStyled id="outlined-basic" label="Phone" size="small" color="secondary" variant="outlined" required />
+                        <TextFieldStyled id="outlined-basic" label="Name" size="small" color="secondary" variant="outlined" />
+                        <TextFieldStyled id="outlined-basic" label="Phone" size="small" color="secondary" variant="outlined" />
                         <TextFieldStyled id="outlined-basic" label="Email Address" size="small" color="secondary" variant="outlined" />
-                        <TextFieldStyled id="outlined-basic" label="Postcode" size="small" color="secondary" variant="outlined" required />
-                        <TextFieldStyled id="outlined-basic" label="Address" size="small" color="secondary" variant="outlined" required />
+                        <TextFieldStyled id="outlined-basic" label="Postcode" size="small" color="secondary" variant="outlined" />
+                        <TextFieldStyled id="outlined-basic" label="Address" size="small" color="secondary" variant="outlined" />
                     </FormWrapper>
                 </Container>
                 <Container>
@@ -28,7 +28,7 @@ export default function CheckoutForm({ basket }) {
                         <Typography variant="h4" component={"h3"} mb={1} sx={{ fontFamily: 'Roboto Condensed, sans-serif' }}>THAT'S GREAT! PLEASE PAY:</Typography>
                         <TitleBox>
                             <Ornament src={require("../../images/ornament-pizza.webp")} alt="" />
-                            <TotalPrice variant="h4" component={"h3"} color={"#e31837"} mx={1.5} mb={1} sx={{ fontFamily: 'Roboto Condensed, sans-serif' }}>
+                            <TotalPrice variant="h4" component={"h3"} color={"#e31837"} mx={1.5} mb={1} sx={{ fontFamily: 'Roboto Condensed, sans-serif' }} data-testid={'component-total-price'}>
                                 £{basket.totalPrice.toFixed(2)}
                             </TotalPrice>
                             <Ornament src={require("../../images/ornament-pizza.webp")} alt="" />
--- pizza-delivery-app/src/Components/__tests__/components/CheckoutForm.test.jsx
+++ pizza-delivery-app/src/Components/__tests__/components/CheckoutForm.test.jsx
@@ -1,3 +1,37 @@
 import CheckoutForm from "../../CheckoutForm";
 import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
 
+describe('checkout form to be filled in with personal details', () => {
+
+    const mockedBasket = { 
+        count: 1, 
+        totalPrice: 8.99 
+    };
+
+    const renderCheckoutForm = () => {
+        render(
+            <CheckoutForm
+                basket = {mockedBasket} 
+            />
+        )
+    }
+
+    it('should render the Component', () => {
+        renderCheckoutForm();
+        expect(screen.getByTestId('component-container')).toBeInTheDocument();
+    })
+
+    it('should display the title text', () => {
+        renderCheckoutForm();
+
+        const titleText = screen.getByTestId('title-text');
+        expect(titleText).toHaveTextContent(/my details/gi);
+    })
+
+    it('should display the totalPrice prop from the basket object', () => {
+        renderCheckoutForm();
+
+        expect(screen.getByTestId('component-total-price')).toHaveTextContent(mockedBasket.totalPrice);
+    })
+}) 
